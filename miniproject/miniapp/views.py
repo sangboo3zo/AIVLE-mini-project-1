6da7e98258efb4eab3f9d19d0c97d8726f9ab11a
yoonjoong <yoonjoong311@gmail.com>
2022-01-25 13:36:37 +0900

    my_cat 추가

--- miniproject/miniapp/views.py
+++ miniproject/miniapp/views.py
@@ -2,7 +2,7 @@ from turtle import st
 from aiohttp import request
 from django.shortcuts import redirect,render
 from django.http import HttpResponse,JsonResponse
-from .models import User,CatPhoto,Cat
+from .models import User,CatPhoto,Cat, UserHasCat
 from django.utils import timezone
 from rest_framework import viewsets
 
@@ -106,12 +106,24 @@ def create_cat(request):
     #         cat_name=cat_name, gender=gender, neutral=neutral,location1=location1, location2=location2, location3=location3, appearance=appearance, status=status)
     #     m.save()
     return render(request, 'miniapp/create_cat.html')
-   
 
 def my_cat(request):
-    user = User.objects.filter(user_no=1).values()
-    cat = Cat.objects.all()
-    return render(request, 'miniapp/my_cat.html',  {'user':user,'cat':cat})
+    name = request.session['id']
+    user=User.objects.get(user_id=name)
+    user_has_cat = UserHasCat.objects.filter(user_no=int(user.user_no))
+    cat_id_list=[i.cat_id for i in user_has_cat]
+    cat_list = [Cat.objects.get(cat_id=i) for i in cat_id_list]
+    cat_img = []
+    print(cat_list)
+    for i in cat_id_list:
+        if CatPhoto.objects.filter(cat_id=i):
+            img_url = CatPhoto.objects.filter(cat_id=i).first().cat_photo
+            cat_img.append(" https://aivle-s43.s3.ap-northeast-2.amazonaws.com/"+ str(img_url))
+        else:
+            cat_img.append("https://aivle-s43.s3.ap-northeast-2.amazonaws.com/no_cat_img.png")
+
+
+    return render(request, 'miniapp/my_cat.html',  {'user':user,'cat':zip(cat_list, cat_img)})
 
 def login_complete(request):
     # if request.method == 'POST':
