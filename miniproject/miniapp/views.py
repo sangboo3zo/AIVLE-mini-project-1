9081788d5028d09c18bbe50bcdac5f58957c5e08
yoonjoong <yoonjoong311@gmail.com>
2022-01-25 15:25:44 +0900

    create_cat 주소 제외 완료

--- miniproject/miniapp/views.py
+++ miniproject/miniapp/views.py
@@ -2,7 +2,7 @@ from turtle import st
 from aiohttp import request
 from django.shortcuts import redirect,render
 from django.http import HttpResponse,JsonResponse
-from .models import User,CatPhoto,Cat, UserHasCat
+from .models import Location, User,CatPhoto,Cat, UserHasCat
 from django.utils import timezone
 from rest_framework import viewsets
 
@@ -76,7 +76,6 @@ def show(request):
 def show2(request,cat_id):
 
     name = request.session['id']
-    print(name)
     c= Cat.objects.get(cat_id=int(cat_id))
     img= CatPhoto.objects.filter(cat_id=int(cat_id))
     #img = CatPhoto.objects.filter(user_no=int(u.user_no))
@@ -92,35 +91,42 @@ def show2(request,cat_id):
 def create_cat(request):
     if request.method == 'POST':
         cat_name = request.POST.get('cat_name')
+        gender = request.POST.get('gender')
+        neutral = request.POST.get('neutral')
+        location = Location.objects.get(location4 ="분당중앙공원")
         appearance = request.POST.get("appearance")
+        status =request.POST.get('status')
+        m = Cat(
+             cat_name=cat_name, gender=gender, neutral=neutral, location=location, appearance=appearance, status=status)
+        m.save()
+
+        name = request.session['id']
+    
+        user = User.objects.get(user_id = name)
+        c = Cat.objects.last()
         
-    #     cat_name = request.POST.get('cat_name')
-    #     gender = request.POST.get('gender')
-    #     neutral = request.POST.get('neutral')
-    #     location1 = request.POST.get('location1')
-    #     location2 = request.POST.get('location2')
-    #     location3 =request.POST.get('location3')
-    #     appearance = request.POST.get('흰색')
-    #     status =request.POST.get('status')
-    #     m = Cat(
-    #         cat_name=cat_name, gender=gender, neutral=neutral,location1=location1, location2=location2, location3=location3, appearance=appearance, status=status)
-    #     m.save()
+        img = request.FILES.get('img-file')
+        time = timezone.now()
+
+        CatPhoto.objects.create(cat_photo=img,date_time=time,user_no_id=user.user_no,cat_id=c.cat_id)
+        return redirect(request, 'miniapp/my_cat.html')
     return render(request, 'miniapp/create_cat.html')
 
 def my_cat(request):
-    name = request.session['id']
-    user=User.objects.get(user_id=name)
-    user_has_cat = UserHasCat.objects.filter(user_no=int(user.user_no))
-    cat_id_list=[i.cat_id for i in user_has_cat]
-    cat_list = [Cat.objects.get(cat_id=i) for i in cat_id_list]
-    cat_img = []
-    print(cat_list)
-    for i in cat_id_list:
-        if CatPhoto.objects.filter(cat_id=i):
-            img_url = CatPhoto.objects.filter(cat_id=i).first().cat_photo
-            cat_img.append(" https://aivle-s43.s3.ap-northeast-2.amazonaws.com/"+ str(img_url))
-        else:
-            cat_img.append("https://aivle-s43.s3.ap-northeast-2.amazonaws.com/no_cat_img.png")
+    if request.method == 'POST':
+        name = request.session['id']
+        user=User.objects.get(user_id=name)
+        user_has_cat = UserHasCat.objects.filter(user_no=int(user.user_no))
+        cat_id_list=[i.cat_id for i in user_has_cat]
+        cat_list = [Cat.objects.get(cat_id=i) for i in cat_id_list]
+        cat_img = []
+        print(cat_list)
+        for i in cat_id_list:
+            if CatPhoto.objects.filter(cat_id=i):
+                img_url = CatPhoto.objects.filter(cat_id=i).first().cat_photo
+                cat_img.append(" https://aivle-s43.s3.ap-northeast-2.amazonaws.com/"+ str(img_url))
+            else:
+                cat_img.append("https://aivle-s43.s3.ap-northeast-2.amazonaws.com/no_cat_img.png")
 
 
     return render(request, 'miniapp/my_cat.html',  {'user':user,'cat':zip(cat_list, cat_img)})
